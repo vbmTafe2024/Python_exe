5533ee36dca2bc7f3ba39043d077a8370865f392
vbmTafe2024 <20136806@tafe.wa.edu.au>
2024-08-27 07:05:49 -0300

    2 python exercise - 27/08/2024

---
+++ Exe_Python02.py
@@ -0,0 +1,20 @@
+
+######################################################################
+#
+# 2.Exercise - User's input
+#
+# Python_exe
+#
+# File:     https://github.com/vbmTafe2024/Python_exe
+# Author:   vbmTafe2024
+# Version:  1.0
+#
+# Copyright:  © Copyright 2024, vbmTafe2024
+#
+# Description: TThis Python code asks the user for their name and
+# then greets them with a personalized message
+#
+######################################################################
+
+Name = input("Enter your name: ")
+print("Hello",Name, "it is a pleasure to have you here :D")
