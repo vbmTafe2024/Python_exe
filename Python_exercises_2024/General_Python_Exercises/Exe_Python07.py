154cbf58435f2147255989b14538df06c0c87e15
20136806 <20136806@tafe.wa.edu.au>
2024-09-01 10:36:00 -0300

    Adding the 7th exercise

---
+++ Python_exercises_2024/General_Python_Exercises/Exe_Python07.py
@@ -0,0 +1,37 @@
+######################################################################
+#
+# 6.Exercise - Multiplication Table
+#
+# Python_exe
+#
+# File:     https://github.com/vbmTafe2024/Python_exe
+# Author:   vbmTafe2024
+# Version:  1.0
+#
+# Copyright:  © Copyright 2024, vbmTafe2024
+#
+# This program helps you to calculate the tables of multiplication
+# (It's a beginner way)
+######################################################################
+first_number =int(input("Enter your number for the multiplication table:"))
+total_1 = first_number*1
+total_2 = first_number*2
+total_3 = first_number*3
+total_4 = first_number*4
+total_5 = first_number*5
+total_6 = first_number*6
+total_7 = first_number*7
+total_8 = first_number*8
+total_9 = first_number*9
+total_10 = first_number*10
+
+print("{} X 1 = {}".format(first_number*1,total_1))
+print("{} X 2 = {}".format(first_number*2,total_2))
+print("{} X 3 = {}".format(first_number*3,total_3))
+print("{} X 4 = {}".format(first_number*4,total_4))
+print("{} X 5 = {}".format(first_number*5,total_5))
+print("{} X 6 = {}".format(first_number*6,total_6))
+print("{} X 7 = {}".format(first_number*7,total_7))
+print("{} X 8 = {}".format(first_number*8,total_8))
+print("{} X 9 = {}".format(first_number*9,total_9))
+print("{} X 10 = {}".format(first_number*10,total_10))
